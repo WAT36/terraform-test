c122f04afa8c0325c1c0ddd876b1b289edee1aad
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2025-05-18 16:33:22 +0900

    SQSを追加

--- main.tf
+++ main.tf
@@ -2,12 +2,10 @@ provider "aws" {
   region = "ap-northeast-1"  # 利用したいAWSリージョン
 }
 
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = "my-terraform-testbucket"  # グローバルに一意な名前を指定
-  acl    = "private"  # バケットのアクセス制御（private, public-read など）
-
-  tags = {
-    Name        = "MyS3Bucket"
-    Environment = "Dev"
-  }
+resource "aws_sqs_queue" "example_queue" {
+  name                      = "my-example-queue"
+  delay_seconds             = 0
+  max_message_size          = 262144   # デフォルト: 256KB
+  message_retention_seconds = 345600   # デフォルト: 4日
+  receive_wait_time_seconds = 10
 }
